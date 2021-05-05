8304ed89f8c46b0cef1aaf0129c1ef629d1e19a2
Walter Nuccio <walter.nuccio@it.clara.net>
2021-05-05 20:30:06 +0200

    complete MovePlayerAcceptanceTest

--- src/test/java/MovePlayerAcceptanceTest.java
+++ src/test/java/MovePlayerAcceptanceTest.java
@@ -9,8 +9,14 @@ public class MovePlayerAcceptanceTest extends BaseAcceptanceTest {
     void a_player_moves_from_start_to_a_new_position() {
         addPlayer("Pippo");
         addPlayer("Pluto");
-        String output = gooseGame.acceptInput("move Pippo 4, 2");
 
+        String output = gooseGame.acceptInput("move Pippo 4, 2");
         assertThat(output).isEqualTo("Pippo rolls 4, 2. Pippo moves from Start to 6");
+
+        String output2 = gooseGame.acceptInput("move Pluto 2, 2");
+        assertThat(output2).isEqualTo("Pluto rolls 2, 2. Pluto moves from Start to 4");
+
+        String output3 = gooseGame.acceptInput("move Pippo 2, 3");
+        assertThat(output3).isEqualTo("Pippo rolls 2, 3. Pippo moves from 6 to 11");
     }
 }
