d0880f7470c0184fe7e09752c7af0c112b3a1fee
Walter Nuccio <walter.nuccio@it.clara.net>
2021-05-05 17:00:14 +0200

    extracat a base class for acceptance tests

--- src/test/java/AddPlayerAcceptanceTest.java
+++ src/test/java/AddPlayerAcceptanceTest.java
@@ -2,41 +2,28 @@ import org.junit.jupiter.api.*;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
-public class AddPlayerAcceptanceTest {
-
-    private static ApplicationRunner applicationRunner;
-
-    @BeforeAll
-    static void setUp() {
-        applicationRunner = new ApplicationRunner();
-        applicationRunner.runApplication();
-    }
-
-    @AfterAll
-    static void tearDown() {
-        applicationRunner.stopApplication();
-    }
+public class AddPlayerAcceptanceTest extends BaseAcceptanceTest {
 
     @Test
     void app_accepts_add_player_command_and_responds_with_an_output() {
-        applicationRunner.acceptInput("add player Pippo");
-        String output = applicationRunner.waitForOutput();
+        gooseGame.acceptInput("add player Pippo");
+        String output = gooseGame.waitForOutput();
 
         assertThat(output).isEqualTo("players: Pippo");
 
-        applicationRunner.acceptInput("add player Pluto");
+        gooseGame.acceptInput("add player Pluto");
 
-        output = applicationRunner.waitForOutput();
+        output = gooseGame.waitForOutput();
         assertThat(output).isEqualTo("players: Pippo, Pluto");
     }
 
     @Test
     void app_rejected_a_duplicated_add_player_command_with_an_error_message() {
-        applicationRunner.acceptInput("add player Pippo");
-        applicationRunner.acceptInput("add player Pippo");
+        gooseGame.acceptInput("add player Pippo");
+        gooseGame.acceptInput("add player Pippo");
 
-        applicationRunner.waitForOutput(); // ignore first output
-        String secondOutput = applicationRunner.waitForOutput();
+        gooseGame.waitForOutput(); // ignore first output
+        String secondOutput = gooseGame.waitForOutput();
 
         assertThat(secondOutput).isEqualTo("Pippo: already existing player");
     }
---
+++ src/test/java/BaseAcceptanceTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+public abstract class BaseAcceptanceTest {
+    protected static ApplicationRunner gooseGame;
+
+    @BeforeAll
+    static void setUp() {
+        gooseGame = new ApplicationRunner();
+        gooseGame.runApplication();
+    }
+
+    @AfterAll
+    static void tearDown() {
+        gooseGame.stopApplication();
+    }
+}
