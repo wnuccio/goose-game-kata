08fe254c93e02fde3d3ec2de5dcc770bc0174d0e
Walter Nuccio <walter.nuccio@it.clara.net>
2021-05-05 16:29:49 +0200

    add special cases on PlayersTest

--- src/test/java/add_player/PlayersTest.java
+++ src/test/java/add_player/PlayersTest.java
@@ -8,6 +8,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class PlayersTest {
 
     @Test
+    void returns_empty_string_for_empty_player_list() {
+        assertThat(new Players().allNamesSeparatedByComma()).isEqualTo("");
+    }
+
+    @Test
+    void returns_just_one_name_for_one_player() {
+        Players onePlayer = new Players().addPlayer("Player1");
+
+        assertThat(onePlayer.allNamesSeparatedByComma()).isEqualTo("Player1");
+    }
+
+    @Test
     void returns_all_player_names_separated_by_comma() {
         Players players = new Players()
                 .addPlayer("Pippo")
