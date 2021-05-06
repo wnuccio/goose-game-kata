c006da7359becc195ec3b967b838b51b3a329b84
Walter Nuccio <walter.nuccio@it.clara.net>
2021-05-06 11:22:48 +0200

    make each AT do a resetGame before execution

--- src/test/java/BaseAcceptanceTest.java
+++ src/test/java/BaseAcceptanceTest.java
@@ -1,18 +1,24 @@
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 public abstract class BaseAcceptanceTest {
     protected static ApplicationRunner gooseGame;
 
     @BeforeAll
-    static void setUp() {
+    static void runApplication() {
         gooseGame = new ApplicationRunner();
         gooseGame.runApplication();
     }
 
     @AfterAll
-    static void tearDown() {
+    static void stopApplication() {
         gooseGame.stopApplication();
+    }
+
+    @BeforeEach
+    void setUp() {
+        resetGame();
     }
 
     protected String addPlayer(String playerName) {
