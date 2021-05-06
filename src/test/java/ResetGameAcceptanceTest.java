8e54e87a0803fc226a5cb7157c2691125b65166a
Walter Nuccio <walter.nuccio@it.clara.net>
2021-05-06 10:40:12 +0200

    change ResetGameAcceptanceTest to verify error message after reset

--- src/test/java/ResetGameAcceptanceTest.java
+++ src/test/java/ResetGameAcceptanceTest.java
@@ -3,21 +3,13 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ResetGameAcceptanceTest extends BaseAcceptanceTest {
-    @Test
-    void move_a_player_on_an_advanced_position() {
-        addPlayer("Pippo");
-
-        String output;
-
-        output = gooseGame.acceptInput("move Pippo 2, 2");
-        assertThat(output).isEqualTo("Pippo rolls 2, 2. Pippo moves from Start to 4");
-    }
 
     @Test
     void after_the_previous_test_a_player_is_newly_on_start() {
-        super.resetGame();
+        resetGame();
 
         String output = gooseGame.acceptInput("move Pippo 2, 2");
-        assertThat(output).isEqualTo("Pippo rolls 2, 2. Pippo moves from Start to 4");
+
+        assertThat(output).isEqualTo("Pippo: no such player");
     }
 }
