88fc95d2944d5edb067a33d607c6cea3e3c3e8df
Walter Nuccio <walter.nuccio@it.clara.net>
2021-05-07 14:19:19 +0200

    change ApplicationRunner to guarantee that a unique thread is created for multiple invocations

--- src/test/java/main/helpers/ApplicationRunner.java
+++ src/test/java/main/helpers/ApplicationRunner.java
@@ -3,9 +3,11 @@ package main.helpers;
 import main.Main;
 
 public class ApplicationRunner {
-    private Thread thread;
+    private static Thread thread = null;
 
     public void runApplication() {
+        if (thread != null) return;
+
         thread = new Thread(() -> Main.main(Main.ARGS_FOR_TEST));
         thread.setDaemon(true);
         thread.start();
