b2163b20edaf2308f126f34ccddd1464cdc6eddd
Walter Nuccio <walter.nuccio@it.clara.net>
2021-05-08 13:19:16 +0200

    replace mock with real class in MovePlayerUseCaseTest

--- src/test/java/usecase/move_player/MovePlayerUseCaseTest.java
+++ src/test/java/usecase/move_player/MovePlayerUseCaseTest.java
@@ -6,41 +6,40 @@ import usecase.Presenter;
 import usecase.add_player.Players;
 
 import static main.test.DiceForTest.dice;
-import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static usecase.move_player.Movement.of;
 
 class MovePlayerUseCaseTest {
-
-    private Players players = mock(Players.class);
+    private Players players = new Players();
     private DiceForTest dice = new DiceForTest(5, 6);
     private Presenter presenter = mock(Presenter.class);
     private MovePlayerUseCase useCase = new MovePlayerUseCase(players, dice, presenter);
 
     @Test
     void moves_a_player_from_start_to_the_specified_position() {
-        when(players.contains("Pippo")).thenReturn(true);
-        when(players.positionOf("Pippo")).thenReturn(0);
+        players.setPositionOf("Pippo", 0);
 
         useCase.acceptCommand(move("Pippo", 4, 2));
 
-        verify(players).setPositionOf("Pippo", 6);
+        assertThat(players.positionOf("Pippo")).isEqualTo(6);
         verify(presenter).presentMovement(of("Pippo").givenRoll(dice(4, 2)).from(0).end());
     }
 
     @Test
     void present_a_victory_when_player_moves_to_the_ending_position() {
-        when(players.contains("Pippo")).thenReturn(true);
-        when(players.positionOf("Pippo")).thenReturn(60);
+        players.setPositionOf("Pippo", 60);
 
         useCase.acceptCommand(move("Pippo", 1, 2));
 
-        verify(players).setPositionOf("Pippo", 63);
+        assertThat(players.positionOf("Pippo")).isEqualTo(63);
         verify(presenter).presentMovement(of("Pippo").givenRoll(dice(1, 2)).from(60).end());
     }
 
     @Test
     void present_an_error_when_the_command_specifies_a_not_present_player() {
-        when(players.contains("Pippo")).thenReturn(false);
+        players.clear();
 
         useCase.acceptCommand(move("Pippo", 1, 2));
 
@@ -49,22 +48,22 @@ class MovePlayerUseCaseTest {
 
     @Test
     void present_a_bouncing_when_player_moves_over_the_ending_position() {
-        when(players.contains("Pippo")).thenReturn(true);
-        when(players.positionOf("Pippo")).thenReturn(60);
+        players.setPositionOf("Pippo", 60);
 
         useCase.acceptCommand(move("Pippo", 2, 3));
 
-        verify(players).setPositionOf("Pippo", 61);
+        assertThat(players.positionOf("Pippo")).isEqualTo(61);
         verify(presenter).presentMovement(of("Pippo").givenRoll(dice(2, 3)).from(60).end());
     }
 
     @Test
     void compute_movement_by_rolling_dice_when_command_does_not_specify_any_value() {
         dice.values(5, 6);
-        when(players.contains("Pippo")).thenReturn(true);
+        players.setPositionOf("Pippo", 0);
 
         useCase.acceptCommand(move("Pippo"));
 
+        assertThat(players.positionOf("Pippo")).isEqualTo(11);
         verify(presenter).presentMovement(of("Pippo").givenRoll(dice(5, 6)).from(0).end());
     }
 
