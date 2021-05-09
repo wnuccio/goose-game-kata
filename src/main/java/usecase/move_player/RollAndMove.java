44b8def95035bebd09914fc19a77330e2febeb9d
Walter Nuccio <walter.nuccio@it.clara.net>
2021-05-09 20:31:25 +0200

    add RollAndMove decorator for MovePlayerUseCase

---
+++ src/main/java/usecase/move_player/RollAndMove.java
@@ -0,0 +1,18 @@
+package usecase.move_player;
+
+public class RollAndMove extends MovePlayerUseCase {
+    private Dice dice;
+    private MovePlayerUseCase movePlayer;
+
+    public RollAndMove(Dice dice, MovePlayerUseCase movePlayer) {
+        super(null, null, null);
+        this.dice = dice;
+        this.movePlayer = movePlayer;
+    }
+
+    @Override
+    public void acceptCommand(MoveCommand command) {
+        MoveCommand commandWithDice = new MoveCommand(command.player(), dice.roll());
+        movePlayer.acceptCommand(commandWithDice);
+    }
+}
---
+++ src/test/java/usecase/move_player/RollAndMoveTest.java
@@ -0,0 +1,29 @@
+package usecase.move_player;
+
+import main.test.DiceForTest;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+class RollAndMoveTest {
+    Dice dice = mock(Dice.class);
+    MovePlayerUseCase movePlayer = mock(MovePlayerUseCase.class);
+    RollAndMove rollAndMove = new RollAndMove(dice, movePlayer);
+
+    @Test
+    void add_a_rolled_dice_to_move_command_when_not_specified() {
+        DiceForTest rolledDice = new DiceForTest(3, 4);
+        when(dice.roll()).thenReturn(rolledDice);
+
+        rollAndMove.acceptCommand(new MoveCommand("Pippo"));
+
+        ArgumentCaptor<MoveCommand> moveCommand = ArgumentCaptor.forClass(MoveCommand.class);
+        verify(movePlayer).acceptCommand(moveCommand.capture());
+        assertThat(moveCommand.getValue().player()).isEqualTo("Pippo");
+        assertThat(moveCommand.getValue().dice()).isEqualTo(Optional.of(rolledDice));
+    }
+}
