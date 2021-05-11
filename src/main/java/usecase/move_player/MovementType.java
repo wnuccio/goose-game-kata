a20ffd94f94d53339c43dd1904f188163d6265a4
Walter Nuccio <walter.nuccio@it.clara.net>
2021-05-11 15:23:14 +0200

    simplify MovementType

--- src/main/java/usecase/move_player/MovementType.java
+++ src/main/java/usecase/move_player/MovementType.java
@@ -1,32 +1,7 @@
 package usecase.move_player;
 
 public enum MovementType {
-    SIMPLE(true, false, false, false),
-    BOUNCING(false, true, false, false),
-    JUMP_ON_BRIDGE(false, false, true, false),
-    REPEAT_ON_GOOSE(false, false, false, true);
-
-    boolean simple;
-    boolean bouncing;
-    boolean jumpOnBridge;
-    boolean repeatOnGoose;
-
-    MovementType(boolean simple, boolean bouncing, boolean jumpOnBridge, boolean repeatOnGoose) {
-        this.simple = simple;
-        this.bouncing = bouncing;
-        this.jumpOnBridge = jumpOnBridge;
-        this.repeatOnGoose = repeatOnGoose;
-    }
-
-    public boolean isBouncing() {
-        return bouncing;
-    }
-
-    public boolean isJumpOnBridge() {
-        return jumpOnBridge;
-    }
-
-    public boolean isRepeatOnGoose() {
-        return repeatOnGoose;
-    }
+    BOUNCING,
+    JUMP_ON_BRIDGE,
+    REPEAT_ON_GOOSE
 }
