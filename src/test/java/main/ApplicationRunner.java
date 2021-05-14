dbb8f91a8482f198912bb4d0d5a9df74e3fc773b
Walter Nuccio <walter.nuccio@it.clara.net>
2021-05-14 17:46:41 +0200

    replace applicationRunning flag with an instance of ApplicationDriver

--- src/test/java/main/ApplicationRunner.java
+++ src/test/java/main/ApplicationRunner.java
@@ -1,33 +1,34 @@
 package main;
 
 public class ApplicationRunner {
-    private static boolean applicationRunning  = false;
+    private static ApplicationDriver driver = null;
 
     public static void runApplication() {
-        if (isApplicationRunning()) return;
+        if (driver != null) return;
 
         Main.injectedBuilder = new GooseGameAppBuilderForTest();
 
         Thread thread = new Thread(ApplicationRunner::invokeMainDetectingCrash);
         thread.setDaemon(true);
         thread.start();
+
     }
 
     private static void invokeMainDetectingCrash() {
         try {
-            applicationRunning = true; // start: the application thread is running
+            driver = new ApplicationDriver(); // start: the application thread is running
             Main.main(new String[]{});
-            applicationRunning = false; // normal termination: the application thread ends
+            driver = null;                    // normal termination: the application thread ends
 
         } catch (Exception e) {
             e.printStackTrace();
-            applicationRunning = false; // crash: the application thread ends
+            driver = null;                    // crash: the application thread ends
         }
     }
 
     public static boolean isApplicationRunning() {
         waitAbit();
-        return applicationRunning;
+        return driver != null;
     }
 
     private static void waitAbit() {
