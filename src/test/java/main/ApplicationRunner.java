91c556681458bfc03aa5445d0a7a40683b2a5bdf
Walter Nuccio <walter.nuccio@it.clara.net>
2021-05-17 11:37:41 +0200

    add check to avoid multiple run invocations in ApplicationRunner

--- src/test/java/main/ApplicationRunner.java
+++ src/test/java/main/ApplicationRunner.java
@@ -3,13 +3,15 @@ package main;
 public class ApplicationRunner {
     private boolean appRunning = false;
 
-    private GooseGameAppBuilder appBuilder;
+    private final GooseGameAppBuilder appBuilder;
 
     public ApplicationRunner(GooseGameAppBuilder appBuilder) {
         this.appBuilder = appBuilder;
     }
 
     public void runApplication() {
+        if (appRunning) return;
+
         Main.injectedBuilder = appBuilder;
 
         Thread thread = new Thread(this::invokeMainDetectingCrash);
