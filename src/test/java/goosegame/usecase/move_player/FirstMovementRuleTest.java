a78667ef6f057310346530536efbf82dc8dd5724
Walter Nuccio <walter.nuccio@it.clara.net>
2021-05-21 19:10:58 +0200

    improve unit test of FirstMovementRule

--- src/test/java/goosegame/usecase/move_player/FirstMovementRuleTest.java
+++ src/test/java/goosegame/usecase/move_player/FirstMovementRuleTest.java
@@ -7,27 +7,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static goosegame.domain.Dice.dice;
-import static goosegame.domain.Position.START;
 import static goosegame.domain.Position.position;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class FirstMovementRuleTest {
     Players players = new Players();
     FirstMovementRule rule = new FirstMovementRule(players);
+    List<Movement> movements = new ArrayList<>();
 
     @Test
-    void build_a_movement_with_player_name_initial_position_and_dice_values() {
-        players.setPositionOf("Pippo", position(0));
+    void build_a_movement_with_start_and_final_positions() {
+        players.setPositionOf("Pippo", position(10));
 
-        List<Movement> movements = new ArrayList<>();
         rule.apply(move("Pippo", 4, 3), movements);
 
-        assertThat(players.positionOf("Pippo")).isEqualTo(position(7));
+        assertThat(players.positionOf("Pippo")).isEqualTo(position(17));
 
-        assertThat(movements.size()).isEqualTo(1);
         Movement movement = movements.get(0);
-        assertThat(movement.startPosition()).isEqualTo(START);
-        assertThat(movement.finalPosition()).isEqualTo(position(7));
+        assertThat(movement.startPosition()).isEqualTo(position(10));
+        assertThat(movement.finalPosition()).isEqualTo(position(17));
     }
 
     public static MoveCommand move(String player, int first, int second) {
