33a222d797c4adddcba97a026ca928cd72e4ebbf
Walter Nuccio <walter.nuccio@it.clara.net>
2021-05-29 14:36:08 +0200

    change GooseRule to implement PositionRule interface

--- src/main/java/app/domain/rules/goose/GooseRule.java
+++ src/main/java/app/domain/rules/goose/GooseRule.java
@@ -1,10 +1,11 @@
 package app.domain.rules.goose;
 
+import app.domain.PositionRule;
 import app.domain.movement.Movement;
 import app.domain.movement.PlayerOnTurn;
 import app.domain.player.Position;
 
-public class GooseRule {
+public class GooseRule implements PositionRule {
 
     public void apply(PlayerOnTurn playerOnTurn) {
         if ( !playerOnTurn.isOnTheGoose()) return;
@@ -16,5 +17,14 @@ public class GooseRule {
         playerOnTurn.applyMovement(movement);
 
         this.apply(playerOnTurn);
+    }
+
+    @Override
+    public void applyTo(PlayerOnTurn playerOnTurn) {
+        Position position = playerOnTurn.position();
+        Position finalPosition = position.plus(playerOnTurn.dice());
+
+        Movement movement = new GooseMovement(position, finalPosition);
+        playerOnTurn.applyMovement(movement);
     }
 }
--- src/test/java/app/domain/rules/goose/GooseRuleTest.java
+++ src/test/java/app/domain/rules/goose/GooseRuleTest.java
@@ -63,4 +63,19 @@ class GooseRuleTest {
 
         verify(playerOnTurn, never()).applyMovement(any());
     }
+
+    @Test
+    void apply_a_movement_based_on_dice_to_the_player() {
+        PlayerOnTurn playerOnTurn = mock(PlayerOnTurn.class);
+        when(playerOnTurn.position()).thenReturn(board.position(5));
+        when(playerOnTurn.dice()).thenReturn(new Dice(3, 4));
+
+        rule.applyTo(playerOnTurn);
+
+        ArgumentCaptor<GooseMovement> movement = ArgumentCaptor.forClass(GooseMovement.class);
+        verify(playerOnTurn).applyMovement(movement.capture());
+
+        assertThat(movement.getValue().startPosition()).isEqualTo(board.position(5));
+        assertThat(movement.getValue().finalPosition()).isEqualTo(board.position(12));
+    }
 }
