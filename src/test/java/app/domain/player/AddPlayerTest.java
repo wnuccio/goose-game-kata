da2d2355caf54238cd0f45425e188d93a312387d
Walter Nuccio <walter.nuccio@it.clara.net>
2021-05-31 14:34:49 +0200

    use a mocked Board in AddPlayerTest

--- src/test/java/app/domain/player/AddPlayerTest.java
+++ src/test/java/app/domain/player/AddPlayerTest.java
@@ -1,6 +1,8 @@
 package app.domain.player;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InOrder;
 import org.mockito.Mockito;
 
@@ -8,18 +10,31 @@ import java.util.Set;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toSet;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 class AddPlayerTest {
     private final Players players = mock(Players.class);
     private final PlayerPresenter presenter = mock(PlayerPresenter.class);
-    private final AddPlayer addPlayer = new AddPlayer(new Board(), players, presenter);
+    private final Board board = mock(Board.class);
+    private AddPlayer addPlayer;
+
+    @BeforeEach
+    void setUp() {
+        addPlayer = new AddPlayer(board, players, presenter);
+    }
 
     @Test
     void add_a_new_player_when_an_add_player_command_is_provided() {
+        Position startPosition = new Position(null, 0, "");
+        when(board.start()).thenReturn(startPosition);
         when(players.allNames()).thenReturn(players("Pippo"));
 
         addPlayer.doAdd("Pippo");
+
+        ArgumentCaptor<Player> player = ArgumentCaptor.forClass(Player.class);
+        verify(players).add(player.capture());
+        assertThat(player.getValue().position()).isEqualTo(startPosition);
 
         verify(presenter).presentPlayers(players("Pippo"));
     }
