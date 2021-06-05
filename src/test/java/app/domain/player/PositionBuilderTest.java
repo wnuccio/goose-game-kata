79885d84093f9bd0e1568a07cc355a72e44fe6ca
Walter Nuccio <walter.nuccio@it.clara.net>
2021-06-05 06:23:42 +0200

    add PositionBuilder test for default construction

--- src/test/java/app/domain/player/PositionBuilderTest.java
+++ src/test/java/app/domain/player/PositionBuilderTest.java
@@ -1,17 +1,25 @@
 package app.domain.player;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static app.domain.player.PositionRule.NO_RULE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
 class PositionBuilderTest {
 
+    private Board board;
+    private PositionBuilder positionBuilder;
+
+    @BeforeEach
+    void setUp() {
+        board = new Board();
+        positionBuilder = new PositionBuilder(board);
+    }
+
     @Test
     void build_a_position_with_specified_attributes() {
-        Board board = new Board();
-        PositionBuilder positionBuilder = new PositionBuilder(board);
-
         int aValue = 7;
         String aName = "aName";
         PositionRule aRule = mock(PositionRule.class);
@@ -23,5 +31,12 @@ class PositionBuilderTest {
                 .build();
 
         assertThat(position.isEqualTo(new Position(board, aValue, aName).attachRule(aRule))).isTrue();
+    }
+
+    @Test
+    void build_a_position_with_default_attributes() {
+        Position position = positionBuilder.build();
+
+        assertThat(position.isEqualTo(new Position(board, 0,"0").attachRule(NO_RULE))).isTrue();
     }
 }
