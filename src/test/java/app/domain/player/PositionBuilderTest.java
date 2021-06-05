d47c580325e6470b72ca2c2c833f7db990152451
Walter Nuccio <walter.nuccio@it.clara.net>
2021-06-05 07:48:27 +0200

    fix test cases names

--- src/test/java/app/domain/player/PositionBuilderTest.java
+++ src/test/java/app/domain/player/PositionBuilderTest.java
@@ -20,7 +20,7 @@ class PositionBuilderTest {
     }
 
     @Test
-    void build_a_position_with_specified_attributes() {
+    void builds_a_position_with_specified_attributes() {
         int aValue = 7;
         String aName = "aName";
         PositionRule aRule = mock(PositionRule.class);
@@ -46,14 +46,14 @@ class PositionBuilderTest {
     }
 
     @Test
-    void build_a_position_with_default_attributes() {
+    void builds_a_position_with_default_attributes() {
         Position position = positionBuilder.build();
 
         assertThat(position.isEqualTo(new Position(board, 0,"0").attachRule(NO_RULE))).isTrue();
     }
 
     @Test
-    void build_a_position_with_a_stringified_value_as_name() {
+    void builds_a_position_with_a_stringified_value_as_name() {
         Position position = positionBuilder
                 .withValue(5)
                 .build();
@@ -62,7 +62,7 @@ class PositionBuilderTest {
     }
 
     @Test
-    void build_a_position_by_overriding_the_default_name() {
+    void builds_a_position_by_overriding_the_default_name() {
         Position position = positionBuilder
                 .withValue(5)
                 .withName("5, The Goose")
